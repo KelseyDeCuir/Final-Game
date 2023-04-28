42327a2a7450388560d862c896b07b3235ffa4c6
crowsCaledfwlch <122137319+crowsCaledfwlch@users.noreply.github.com>
2023-04-28 13:47:38 -0400

    Started Personality

--- StarterGame/GenClasses/Personality.cs
+++ StarterGame/GenClasses/Personality.cs
@@ -8,4 +8,14 @@ namespace Ascension
     {
         COWARD, AGRESSIVE, KIND, BRAVE
     }
+    public class CPersonality
+    {
+        static Dictionary<Command, double> cowardCommands = new Dictionary<Command, double>() { };
+        static Dictionary<Command, double> agroCommands = new Dictionary<Command, double>() { };
+        static Dictionary<Command, double> kindCommands = new Dictionary<Command, double>() { };
+        static Dictionary<Command, double> braveCommands = new Dictionary<Command, double>() { };
+
+        Dictionary<Personality, Dictionary<Command, double>> personalities = new Dictionary<Personality, Dictionary<Command, double>>() { { Personality.COWARD, cowardCommands }, { Personality.AGRESSIVE, agroCommands }, { Personality.KIND, kindCommands }, {Personality.BRAVE, braveCommands } };
+    }
+
 }
